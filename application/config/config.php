2dd30d0555d3f80bf3c9761f7ba194ac44e2dfdc
triteguh <104341044+triteguh@users.noreply.github.com>
2022-04-27 23:32:52 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = $root;
-$config['base_url'] = 'http://localhost/pustaka-booking/';
+$config['base_url'] = 'http://localhost/WP2_TRITEGUH/';
 
 /*
 |--------------------------------------------------------------------------
